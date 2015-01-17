9f8255731d58e8663ee4a5e266146f75028e736a
Sturm Flut <sturmflut@lieberbiber.de>
2015-01-17 12:59:20 +0100

    Clean up the code

--- main.c
+++ main.c
@@ -21,28 +21,32 @@ int main(int argc, char** argv)
     struct icmphdr icmp_hdr;
     char packetdata[sizeof(icmp_hdr) + 5];
 
+    // Create a datagram ICMP socket
     int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
 
     if(sock < 0)
     {
-        printf("sock() errno: %i\n", errno);
+        printf("socket() errno: %i\n", errno);
 
 	return EXIT_FAILURE;
     }
 
-
+    // Initialize the destination address to localhost
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(0x7F000001);
 
+    // Initialize the ICMP header
     memset(&icmp_hdr, 0, sizeof(icmp_hdr));
     icmp_hdr.type = ICMP_ECHO;
     icmp_hdr.un.echo.id = 1234;
     icmp_hdr.un.echo.sequence = 1;
 
+    // Initialize the packet data (header and payload)
     memcpy(packetdata, &icmp_hdr, sizeof(icmp_hdr));
     memcpy(packetdata + sizeof(icmp_hdr), "12345", 5);
 
+    // Send the packet
     if(sendto(sock, packetdata, sizeof(packetdata), 0, (struct sockaddr*) &addr, sizeof(addr)) < 0)
     {
         printf("sendto() errno: %i\n", errno);
@@ -50,8 +54,7 @@ int main(int argc, char** argv)
 	return EXIT_SUCCESS;
     }
     
-
-    printf("ICMP sent");
+    printf("ICMP ECHO packet sent successfully\n");
 
     return EXIT_SUCCESS;
 }
